5e558c011638b0c37911aca128df6d189bbc5c5e
Gaurav Gautam <gautamgaurav2063@gmail.com>
2025-08-05 07:41:37 +0545

    update  work

--- Lab6/virtual.cpp
+++ Lab6/virtual.cpp
@@ -1,43 +1,46 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 class Polygon
 {
-    protected:
-        int width,height;
-    public:
-        void setData(int a,int b )
-        {
-            width=a;
-            height=b;
-        }    
-        virtual int Area()=0;
+protected:
+    int width, height;
+
+public:
+    void setData(int a, int b)
+    {
+        width = a;
+        height = b;
+    }
+    virtual int Area() = 0;
 };
-class Rectangle: public Polygon 
+class Rectangle : public Polygon
 {
-    public:
-        int Area()
-        {
-            cout<<"Area of Rectangle:"<<(width*height)<<endl;
-            return 0;
-        }
+public:
+    int Area()
+    {
+        cout << "Area of Rectangle:" << (width * height) << endl;
+        return 0;
+    }
 };
-class Triangle: public Polygon 
+class Triangle : public Polygon
 {
-    public:
-        int Area(){
-            cout<<"Area of Triangle:"<<(width*height)/2<<endl;
-            return 0;
-        }
+public:
+    int Area()
+    {
+        cout << "Area of Triangle:" << (width * height) / 2 << endl;
+        return 0;
+    }
 };
-int main(){
+int main()
+{
     Polygon *p;
-    Rectangle r; 
+    Rectangle r;
     Triangle t;
-    p=&r;
-    p->setData(10,20);
+    p = &r;
+    p->setData(10, 20);
     p->Area();
-    p=&t;
-    p->setData(10,20);
+    p = &t;
+    p->setData(10, 20);
     p->Area();
     return 0;
 }
