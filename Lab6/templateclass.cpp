c5d86d37f9b605c1b1726e587be2fff18faa8064
Gaurav Gautam <gautamgaurav2063@gmail.com>
2025-08-11 09:56:42 +0545

    update  work

--- Lab6/templateclass.cpp
+++ Lab6/templateclass.cpp
@@ -4,15 +4,22 @@ template <class T>
 class mypair
 {
 private:
-    t a,b;
+    T a, b;
+
 public:
-    mypair(T first, T second) 
+    mypair(T first, T second)
     {
-        a= first;
-        b=second;
+        a = first;
+        b = second;
     }
     T getmax()
     {
-        return a>b?a:b;
+        return a > b ? a : b;
     }
 };
+int main()
+{
+    mypair<float> m1(20.9, 20.1);
+    cout << m1.getmax() << " is greater. " << endl;
+    return 0;
+}
